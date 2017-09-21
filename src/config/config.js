81bc2d99c3c0e74d69e92a06e00da2bbe9e0d20c
Michał Sypko <m.sypko@livechatinc.com>
2017-09-21 02:00:04 +0200

    show email for online people

---
+++ src/config/config.js
@@ -0,0 +1,9 @@
+export default {
+  authProvider: "google",
+  apiKey: "AIzaSyBE54K9b9Efk8qGRq95b5evOwJUuixCWUg",
+  authDomain: "dls-octopus.firebaseapp.com",
+  databaseURL: "https://dls-octopus.firebaseio.com",
+  projectId: "dls-octopus",
+  storageBucket: "dls-octopus.appspot.com",
+  messagingSenderId: "991638903953"
+};
