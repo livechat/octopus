81dd1406461334151fd05d409f2349bc771a6d20
Michał Sypko <m.sypko@livechatinc.com>
2017-09-21 18:35:44 +0200

    tooltips howto in new page template

--- src/templates/new-page.js
+++ src/templates/new-page.js
@@ -12,4 +12,17 @@ A->B
 B->C
 C->A
 }
-[/diagram]`;
+[/diagram]
+
+## Tooltips
+
+<span data-tooltip="I'm a tooltip text!">Basic tootlip</span>
+<span data-tooltip="I'm a tooltip text!" class="tooltip"><code>.tooltip</code> tootlip</span>
+<span data-tooltip="I'm a tooltip text!" class="tooltip-top"><code>.tooltip-top</code> tootlip</span>
+<span data-tooltip="I'm a tooltip text!" class="tooltip-right"><code>.tooltip-right</code> tootlip</span>
+<span data-tooltip="I'm a tooltip text!" class="tooltip-bottom"><code>.tooltip-bottom</code> tootlip</span>
+<span data-tooltip="I'm a tooltip text!" class="tooltip-left"><code>.tooltip-left</code> tootlip</span>
+<span data-tooltip="I'm a tooltip text!" class="tooltip-200"><code>.tooltip-200</code> tootlip</span>
+<span data-tooltip="I'm a tooltip text!" class="tooltip-300"><code>.tooltip-300</code> tootlip</span>
+<span data-tooltip="I'm a tooltip text!" class="tooltip-200 tooltip-right"><code>.tooltip-right.tooltip-200</code> tootlip</span>
+`;
