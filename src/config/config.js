991eece2a8d5f9cfed2768fa68b091759fc77146
Michał Sypko <m.sypko@livechatinc.com>
2017-09-21 19:06:06 +0200

    remove config.js

--- src/config/config.js
+++
@@ -1,9 +0,0 @@
-export default {
-  authProvider: "google",
-  apiKey: "AIzaSyBE54K9b9Efk8qGRq95b5evOwJUuixCWUg",
-  authDomain: "dls-octopus.firebaseapp.com",
-  databaseURL: "https://dls-octopus.firebaseio.com",
-  projectId: "dls-octopus",
-  storageBucket: "dls-octopus.appspot.com",
-  messagingSenderId: "991638903953"
-};
