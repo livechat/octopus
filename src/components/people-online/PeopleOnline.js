b81146e02f54a22c3c4298724b0d4b694824af16
Michał Sypko <m.sypko@livechatinc.com>
2017-09-22 10:29:25 +0200

    <span> instead of <a> elements

--- src/components/people-online/PeopleOnline.js
+++ src/components/people-online/PeopleOnline.js
@@ -61,7 +61,7 @@ Active in the last 5 minutes:
  Who | Where | Last Active
 :--: | :---- | :----------
 ${this.state.users.map((user) => {
-  return [`<a href="#" class="tooltip-right tooltip-200" data-tooltip="${user.email}"><img src="${user.photoUrl}" alt="${user.email}" class="user-avatar" /></a>`, `${window.location.origin}${user.path}`, `${distanceInWordsToNow(user.lastActive)} ago`].join('|');
+  return [`<span class="tooltip-right tooltip-200" data-tooltip="${user.email}"><img src="${user.photoUrl}" alt="${user.email}" class="user-avatar" /></span>`, `${window.location.origin}${user.path}`, `${distanceInWordsToNow(user.lastActive)} ago`].join('|');
 }).join("\n")}
 `}
 </Markdown>
--- src/components/person/Person.js
+++ src/components/person/Person.js
@@ -3,15 +3,14 @@ import React, {Component} from 'react';
 export default class Person extends Component {
   render() {
     return (
-      <a
-        href="#"
+      <span
         className="tooltip-200"
         data-tooltip={this.props.data.email} >
           <img
             src={this.props.data.photoUrl}
             alt={this.props.data.email}
             className="user-avatar" />
-      </a>
+      </span>
     );
   }
 }
