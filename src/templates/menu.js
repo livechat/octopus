e7cc7f04e654380e4dc4c437ba83cd9d74219bdf
Michał Sypko <m.sypko@livechatinc.com>
2018-08-09 19:16:17 +0200

    comment in menu template

--- src/templates/menu.js
+++ src/templates/menu.js
@@ -2,7 +2,7 @@ export default `* <details><summary>Company (collapisble)</summary>
 	* [Company structure](/company-structure)
 	* [Product roadmap](/roadmap)
 </details>
-
+<!-- Double new line here is necessary! -->
 
 * Projects
 	* [Project #1](/project-1)
