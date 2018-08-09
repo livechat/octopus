580963c2b5307c4c248507561281d6a27af5247c
Michał Sypko <m.sypko@livechatinc.com>
2018-08-09 18:58:43 +0200

    collapsible menu template

--- src/templates/menu.js
+++ src/templates/menu.js
@@ -1,6 +1,9 @@
-export default `* Company
+export default `* <details><summary>Company (collapisble)</summary>
 	* [Company structure](/company-structure)
 	* [Product roadmap](/roadmap)
+</details>
+
+
 * Projects
 	* [Project #1](/project-1)
 	* [Project #2](/project-2)
