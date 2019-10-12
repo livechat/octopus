7c843a744d0584af5efc62bfca1cd98b6d52e8d1
Kamil Rudnicki <kamil.rudnicki@gmail.com>
2019-10-12 11:09:28 +0200

    Config & firebase version

---
+++ src/config/config.js
@@ -0,0 +1,11 @@
+export default {
+    authProvider: "google",
+    apiKey: "AIzaSyCzNQesB9ThHSIxQfzdq8ibGfqu_g-vTBA",
+    authDomain: "timecamp-internal.firebaseapp.com",
+    databaseURL: "https://timecamp-internal.firebaseio.com",
+    projectId: "timecamp-internal",
+    storageBucket: "timecamp-internal.appspot.com",
+    messagingSenderId: "923165484790",
+    appId: "1:923165484790:web:dcb0149353d7dc9661aed7",
+    measurementId: "G-PZ6YQ8Q2B6"
+};
